e192807af4156b5cea0d819f9c4ab50cb0a39b99
unknown <ayodelejoseph0613@gmail.com>
2026-08-12 11:54:24 -0500

    python strings exercise completed

---
+++ first_lesson.py
@@ -0,0 +1,4 @@
+what_are_you_learning = "I am learning Python programming!"
+
+
+print(what_are_you_learning)
---
+++ python_strings.py
@@ -0,0 +1,59 @@
+'''# Exercise 1: Printing python strings with single quote and double qoutes
+print ("Hello") # this is a double quotes
+print ('Hello')'''
+
+
+
+'''# Exercise 2: using quotes inside a string
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johonny"')'''
+
+
+
+'''# Exercise 3: assign string to a variable
+a = "Hello"
+print(a)'''
+
+
+
+'''# Exercise 4: assign a multiline string to a variable by using three quotes
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)'''
+
+
+'''# Exercise 5: to get the character at position 1
+a = "hallo,World!"
+print (a[1])'''
+
+
+'''# Exercise 5: looping through string
+for x in "banana":
+  print(x)'''
+
+
+
+# Exercise 6: string length
+a = "Hallo, World!" 
+print(len(a))
+
+
+
+'''# Exercise 7: to check the string
+txt = "the best things in life are free!" 
+print ("free" in txt)'''
+
+
+
+'''# Exercise 8: to check if a certain phrase
+txt = "The best things in life are free!"
+print ("expensive" not in txt)'''
+
+
+
+'''#Exercise 9:to index letter
+x = 'Welcome'
+print(x[3])'''
