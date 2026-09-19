bed68d3e7a893eaa1578eb528ca684d952be6707
unknown <ayodelejoseph0613@gmail.com>
2026-09-19 10:52:49 +0100

    python boolean exercise

---
+++ python_bool.py
@@ -0,0 +1,15 @@
+#Exercise 1: print the result of 10 > 9
+print(10 > 9)
+# Answer: True 
+
+#Exercise 2: print the result of 10 == 9
+print(10 == 9)
+# Answer: false
+
+#Exercise 3: print the result of boll("Hallo")
+print(bool("Hall0"))
+# Answer: True
+
+#Exercise 4: print the result of bool(0)
+print(bool(0))
+# Answer: false
